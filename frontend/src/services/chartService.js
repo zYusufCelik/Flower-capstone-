7bf8bbc8189b3ed0d37b65dcc514208e7be4677d
yağmur çelik <yagmurcelik01@gmail.com>
2025-03-28 22:44:25 +0300

    Axios instance ve servis oluşturuldu

---
+++ frontend/src/services/chartService.js
@@ -0,0 +1,23 @@
+import axiosInstance from "./axiosInstance";
+
+// Chart kaydetme
+export const saveChart = async (chartRequest) => {
+    try {
+      const response = await axiosInstance.post('/saveChart', chartRequest);
+      return response.data;
+    } catch (error) {
+      console.error('Chart kaydetme hatası:', error);
+      throw error;
+    }
+  };
+  
+  // Chart silme
+  export const deleteChart = async (id) => {
+    try {
+      const response = await axiosInstance.delete(`/deleteChart/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error('Chart silme hatası:', error);
+      throw error;
+    }
+  };
